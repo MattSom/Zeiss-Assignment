bbf59bff9ef88debd0df51c2380d0c6cd851f7cd
MattSom <somesz@gmail.com>
2023-10-15 00:31:02 +0200

    refact(StackStorage): Add `setter` to `stacks` property
    **Scope**
     - A `StackStorage` should not have stacks that are not numbered from 1
       without gaps.
    
    **Development**
     - Refactored `stacks` to be a `@property` with a custom setter that
       validates the stack list we want to assign to it.
    
    **Guide**
     - Check unit tests.

--- app/models/storage/stack_storage.py
+++ app/models/storage/stack_storage.py
@@ -8,7 +8,24 @@ from .storage import Storage
 class StackStorage(Storage):
     def __init__(self, limit=0, stacks=[]):
         self.limit = limit
+        self._stacks = []
         self.stacks = stacks
+
+    @property
+    def stacks(self):
+        return self._stacks
+
+    @stacks.setter
+    def stacks(self, stacks):
+        expected_numbers = list(range(1, len(stacks) + 1))
+        actual_numbers = sorted([stack.number for stack in stacks])
+
+        if expected_numbers != actual_numbers:
+            raise ValueError(
+                "Stacks must be numbered consecutively starting from 1 without any gaps."
+            )
+
+        self._stacks = stacks
 
     def __repr__(self):
         storage_repr = ""
--- app/models/storage/tests/test_stack_storage.py
+++ app/models/storage/tests/test_stack_storage.py
@@ -14,6 +14,24 @@ class TestStackStorage:
     def setup(self):
         self.storage = StackStorage(limit=10)
 
+    def test_setting_valid_stacks_passes(self):
+        valid_stacks = [Stack(number=1), Stack(number=2), Stack(number=3)]
+        self.storage.stacks = valid_stacks
+
+    @pytest.mark.parametrize(
+        "invalid_stacks",
+        [
+            [Stack(number=1), Stack(number=3), Stack(number=4)],
+            [Stack(number=2), Stack(number=3), Stack(number=4)],
+        ],
+    )
+    def test_setting_invalid_stacks_raises_ValueError(self, invalid_stacks):
+        with pytest.raises(
+            ValueError,
+            match="Stacks must be numbered consecutively starting from 1 without any gaps.",
+        ):
+            self.storage.stacks = invalid_stacks
+
     def test_add_container_appends_stack(self):
         stack = Stack(number=1)
         self.storage.add_container(stack)
@@ -22,7 +40,9 @@ class TestStackStorage:
     def test_add_container_raises_ValueError_for_appending_stack_beyond_storage_limit(
         self,
     ):
-        self.storage.stacks = [Stack(number=i) for i in range(self.storage.limit)]
+        self.storage.stacks = [
+            Stack(number=i) for i in range(1, self.storage.limit + 1)
+        ]
 
         with pytest.raises(
             ValueError,
